c69c7529cdfc46b3c0d2ced77999b2e368b696b6
Drago09t <92865043+Drago09t@users.noreply.github.com>
2023-04-21 05:18:55 +0500

    Added buttons

--- Commands/Music/play.js
+++ Commands/Music/play.js
@@ -1,4 +1,4 @@
-const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
+const { ButtonBuilder, ActionRowBuilder, EmbedBuilder, SlashCommandBuilder, ButtonStyle } = require("discord.js");
 const client = require("../../index");
 
 module.exports = {
@@ -20,24 +20,69 @@ module.exports = {
     const embed = new EmbedBuilder();
 
     if (!voiceChannel) {
-      embed.setColor("Red").setDescription("You must be in a voice channel to execute music commands.");
+      embed.setColor("#ff0000").setDescription("You must be in a voice channel to execute music commands.");
       return interaction.reply({ embeds: [embed], ephemeral: true });
     }
 
     if (!member.voice.channelId == guild.members.me.voice.channelId) {
-      embed.setColor("Red").setDescription(`You can't use the music player as it is already active in <#${guild.members.me.voice.channelId}>`);
+      embed.setColor("#ff0000").setDescription(`You can't use the music player as it is already active in <#${guild.members.me.voice.channelId}>`);
       return interaction.reply({ embeds: [embed], ephemeral: true });
     }
 
     try {
 
       client.distube.play(voiceChannel, query, { textChannel: channel, member: member });
-      return interaction.reply({ content: "🎶 Request received." });
+
+      const pauseButton = new ButtonBuilder()
+        .setCustomId("pause")
+        .setLabel("Pause")
+        .setStyle(ButtonStyle.Secondary);
+
+
+      const resumeButton = new ButtonBuilder()
+        .setCustomId("resume")
+        .setLabel("Resume")
+        .setStyle(ButtonStyle.Secondary);
+
+      const skipButton = new ButtonBuilder()
+        .setCustomId("skip")
+        .setLabel("Skip")
+        .setStyle(ButtonStyle.Danger);
+
+      const row = new ActionRowBuilder()
+        .addComponents(pauseButton, resumeButton, skipButton);
+
+      const message = await interaction.reply({
+        content: "🎶 Request received.",
+        components: [row]
+      });
+
+      // Add a listener for button interactions
+      const filter = (i) => ["pause", "resume", "skip"].includes(i.customId) && i.user.id === interaction.user.id;
+      const collector = message.createMessageComponentCollector({ filter, time: 1500000 });
+
+      collector.on("collect", async (i) => {
+        if (i.customId === "pause") {
+          client.distube.pause(guild);
+          await i.update({ content: "⏸ Music paused.", components: [row] });
+        } else if (i.customId === "resume") {
+          client.distube.resume(guild);
+          await i.update({ content: "▶️ Music resumed.", components: [row] });
+        } else if (i.customId === "skip") {
+          client.distube.skip(guild);
+          await i.update({ content: "⏭️ Song skipped.", components: [row] });
+        }
+      });
+
+      collector.on("end", () => {
+        // Remove the buttons after the collector expires
+        message.edit({ components: [] });
+      });
 
     } catch (err) {
       console.log(err);
 
-      embed.setColor("Red").setDescription("⛔ | Something went wrong...");
+      embed.setColor("#ff0000").setDescription("⛔ | Something went wrong...");
 
       return interaction.reply({ embeds: [embed], ephemeral: true });
     }
